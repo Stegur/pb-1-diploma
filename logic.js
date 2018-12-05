082396f6d2c9d48aec0bdf78ec6fa479c5def944
Stegura Alex <stegura.alex@gmail.com>
2018-12-05 22:54:02 +0300

    Создание игры без проверки на победу

--- logic.js
+++ logic.js
@@ -1,2 +1,37 @@
 let players = ['x', 'o'];
 let activePlayer = 0;
+
+let counter = 0;
+
+let field = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
+
+function startGame() {
+
+    field = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ];
+    renderBoard(field);
+}
+
+function click(row, col) {
+
+    if (counter % 2 === 0) {
+        activePlayer = 0
+    } else {
+        activePlayer = 1
+    }
+
+
+    field[row][col] = players[activePlayer];
+    renderBoard(field);
+
+
+    counter++;
+
+}
